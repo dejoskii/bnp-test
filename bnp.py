dba1bdd905785550588485babaff80167d26787b
dejoskii <dejiok76@gmail.com>
2019-08-11 20:32:36 +0100

    # V3-Final Code with Unit test

--- bnp.py
+++ bnp.py
@@ -1,111 +1,117 @@
 import pandas as pd
 import xml.etree.ElementTree as et
 import datetime
+import sys
 
+class BnpTest():
 
-# log file
-logfile = "Server.log"
-
-
-class bnpTest():
-
-    def __init__(self, input_file=None, corid=None):
+    def __init__(self,corid=None):
         self.corid = corid
         self.pd_list = []
-        self.input_file = input_file
-        #self.logger = log_file(txt)
+        self.df = []
+        self.corid = []
+        self.df_list = []
+        self.input_file = "input.xml"
+        self.logfile = "Server.log"
+        self.output_file = "results.csv"
 
     def log_file(self, text):
         """ This is the loggin function. It writes the outcome to the server.log file"""
-        with open(logfile, "a+") as text_file:
+        with open(self.logfile, "a+") as text_file:
             text_file.write('\n' + datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p") + '\n')
             text_file.write("==========================\n")
             text_file.write(text + "\n")
 
-    def create_df_list(self, input_file):
+    def create_df_list(self,input_file):
         """ This Method is used to create a list based on the input file obtained
                 The list will then be used to create a pandas DF object
                 """
         self.input_file = input_file
 
-        df_list = []
-
     # extract values from xml file using etree
         try:
-            input_xml = self.input_file
-            xml_root = et.parse(input_xml).getroot()
-
-            for node in xml_root:
+            xml_root = et.parse(self.input_file).getroot()
+            for item in xml_root:
                 df_dict = {}
-                df_dict["CorrelationID"] = node.attrib.get("GPID")
-                df_dict["NumberOfTrades"] = (int(node.find("NumberOfTrades").text))
-                df_dict["Limit"] = (int(node.find("Limit").text))
-                df_dict["Value"] = (int(node.find("Value").text))
-                df_dict["TradeID"] = node.find("TradeID").text
-
-                df_list.append(df_dict)
-            return df_list
+                for ele in item:
+                    df_dict[ele.tag] = ele.text
+                self.df_list.append(df_dict)
+            return self.df_list
 
         except ValueError as e:
             txt = "The Value of this item should be an Integer"
-            bnpTest.log_file(self, txt)
-
+            BnpTest.log_file(self, txt)
         except FileNotFoundError:
             txt = "The input xml file cant be found. Ensure its in the same directory\n"
-            bnpTest.log_file(self, txt)
-
+            BnpTest.log_file(self, txt)
         except Exception as e:
             txt = f"{e}"
-            bnpTest.log_file(self, txt)
+            BnpTest.log_file(self, txt)
 
     def create_df_series(self):
-        """ This is the class method that creates the Pandas Data Frame."""
+        """ This is the class method that creates the Pandas Data Frame. The df.apply(pd.to_numeric, converts
+        the relevant series in the data frame to integers"""
+
         df_cols = ["CorrelationID", "NumberOfTrades", "Limit", "Value", "TradeID"]
-        df = pd.DataFrame(self.create_df_list("input.xml"), columns=df_cols)
+        df = pd.DataFrame(self.df_list, columns=df_cols)
+        df_cols_drop = df.columns.drop(['CorrelationID', 'TradeID'])  # drop columns that are strings
+        df[df_cols_drop] = df[df_cols_drop].apply(pd.to_numeric, errors="coerce")
+        self.df.append(df)
         corid = [x for x in df.CorrelationID[::]]
-        return df, corid
+        self.corid.append(corid)
+        return corid, df
 
     def aggregate_df_corid(self):
         """ This is my class method that aggregates the data frame series. and prints the state of the orders"""
-        df_order, corid = self.create_df_series()[0], self.create_df_series()[1]
-        try:
+        df_order, corid = self.df[0], self.corid[0]
 
+        try:
             for corid in sorted(set(corid)):
                 df_aggr = df_order[df_order.CorrelationID == corid]
-                total_value = df_aggr['Value'].sum()
-                new_df = df_aggr.iloc[0:1, 0:]
-                new_df['Value'] = total_value
+                total_value = df_aggr["Value"].sum()
+                new_df = df_aggr.iloc[0:1, 0:]  # get the first row in the series
+                new_df["Value"] = total_value
                 if (df_aggr.NumberOfTrades > df_aggr.NumberOfTrades.count()).all():
                     new_df["State"] = "Pending"
                 elif (new_df["Value"] > new_df["Limit"]).all():
                     new_df["State"] = "Rejected"
                 elif (new_df["Value"] <= new_df["Limit"]).all():
                     new_df["State"] = "Accepted"
-
                 filtered_df = new_df.drop(new_df.columns[2:5], 1)
-                self.pd_list.append(filtered_df)
-            #return self.pd_list
-            bnpTest.write_to_csv(self)
+                self.pd_list.append(filtered_df) # Add each filtered DF to the empty list
+            return self.pd_list
 
         except Exception as e:
             txt = f"{e}"
-            bnpTest.log_file(self, txt)
+            BnpTest.log_file(self, txt)
 
-    def write_to_csv(self):
+    def write_to_csv(self, output_file):
+        self.output_file = output_file
         try:
-            final_df = pd.concat(self.pd_list)
-            final_df.to_csv("results.csv", index=None)
+            final_df = pd.concat(self.pd_list, ignore_index=True) # Add all DF in the list to form one single DF
+            final_df.to_csv(self.output_file, index=None)
             return final_df
 
         except ValueError as e:
             txt = f"Looks like something went wrong. Please check server logs file for more details. {e}" + "\n"
-            bnpTest.log_file(self, txt)
+            BnpTest.log_file(self, txt)
 
         except Exception as e:
             txt = f"{e}"
-            bnpTest.log_file(self, txt)
+            BnpTest.log_file(self, txt)
         finally:
             txt = "Requested result.csv file successfully written\n"
-            bnpTest.log_file(self, txt)
+            BnpTest.log_file(self, txt)
+
+    def call_all(self):
+        bnp_orders.create_df_list(sys.argv[1])
+        bnp_orders.create_df_series()
+        bnp_orders.aggregate_df_corid()
+        bnp_orders.write_to_csv("results.csv")
 
 
+if len(sys.argv) < 2:
+    print('usage : python bnp_test.py input.xml ')
+else:
+    bnp_orders = BnpTest()
+    bnp_orders.call_all()
--- test_bnp.py
+++ test_bnp.py
@@ -1,35 +1,67 @@
-import unittest
-from bnp import bnpTest
+from twisted.trial import unittest
+from bnp import BnpTest
+import pandas as pd
 import os
 
 class TestBnpSolution(unittest.TestCase):
 
     def setUp(self):
-        self.obj = bnpTest()
-
-        self.result_csv = 'results.csv'
+        self.obj = BnpTest()
+        self.xml = "input.xml"
+        self.result_csv = "results.csv"
+        self.load_file = self.obj.create_df_list(self.xml)
 
     def test_create_df_list(self):
-        self.df_list = self.obj.create_df_list("input.xml")
+        self.df_list = self.obj.create_df_list(self.xml)
         df_headers = self.df_list[0].keys()
-        self.assertIn('CorrelationID', df_headers)
-        self.assertIn('NumberOfTrades', df_headers)
-        self.assertIn('Value', df_headers)
+        self.assertIn("CorrelationID", df_headers)
+        self.assertIn("NumberOfTrades", df_headers)
+        self.assertIn("Value", df_headers)
         self.assertEqual(self.df_list[4]['CorrelationID'], "200")
-        self.assertEqual(self.df_list[4]['NumberOfTrades'], 2)
-        self.assertEqual(self.df_list[4]['Limit'], 2000)
+        self.assertEqual(self.df_list[4]['NumberOfTrades'], "2")
+        self.assertEqual(self.df_list[4]['Limit'], "2000")
+
+    def test_create_df_corid(self):
+
+        corid_list = ["234", "234", "222", "234", "200", "200"]
+        test_cordid_list = self.obj.create_df_series()[0]
+        self.assertListEqual(corid_list, test_cordid_list)
+
+    def test_create_series(self):
+
+        expected_df = pd.read_csv("expected_df.csv") # The data frame series are integers at this point
+        expected_df=expected_df.astype(str)  # convert everything to string
+        df_drop = expected_df.columns.drop(["CorrelationID", "TradeID"])  # drop columns that we need to remain strings
+        # convert the rest to integers
+        expected_df[df_drop] = expected_df[df_drop].apply(pd.to_numeric, errors ="coerce")
+        print(expected_df)
+        print(type(expected_df.iloc[0, 0]))
+        print(type(expected_df.iloc[0,3]))
+        my_df = self.obj.create_df_series()[1]
+        pd.testing.assert_frame_equal(my_df, expected_df)
+
+    def test_aggregate_df_corid(self):
+        aggr_list = []
+        aggr_1 = pd.read_csv("data1.csv")
+        aggr_list.append(aggr_1)
+        aggr_2 = pd.read_csv("data2.csv")
+        aggr_list.append(aggr_2)
+        aggr_3 = pd.read_csv("data3.csv")
+        aggr_list.append(aggr_3)
+        expected_aggr_df = pd.concat(aggr_list, ignore_index=True)
+        first_df = self.obj.create_df_series()[1]
+        second_df = self.obj.aggregate_df_corid()
+        final_df = self.obj.write_to_csv("test_output.csv")
+        expected_aggr_df = expected_aggr_df.astype(str)
+        expected_aggr_drop = expected_aggr_df.columns.drop(["CorrelationID","State"])
+        expected_aggr_df[expected_aggr_drop] = expected_aggr_df[expected_aggr_drop].apply(pd.to_numeric)
+        pd.testing.assert_frame_equal(final_df, expected_aggr_df)
 
 
-    def test_final_df(self):
-        self.final_df2 = self.obj.aggregate_df_corid()
-        self.final_df = self.obj.write_to_csv()
 
-        self.assertEqual(self.final_df["State"][self.final_df["CorrelationID"] == "200"].iloc[0],"Pending")
-        self.assertEqual(self.final_df["State"][self.final_df["CorrelationID"] == "222"].iloc[0], "Rejected")
-        self.assertEqual(self.final_df["State"][self.final_df["CorrelationID"] == "234"].iloc[0], "Accepted")
 
-    def test_verify_csv(self):
-        path = os.path.exists(self.result_csv)
-        self.assertTrue(path)
+
+
+
 
 
